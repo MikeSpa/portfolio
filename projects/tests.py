bed0064dfbaaa52b500f333d45490f7721c25b49
Spahr Michael <spahr.michael@protonmail.com>
2023-11-10 16:12:47 +0100

    add tests for projects

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,48 @@
-# from django.test import TestCase
+from django.test import TestCase
+from .models import Project
 
-# Create your tests here.
+
+class ModelTesting(TestCase):
+    def setUp(self):
+        self.blog = Project.objects.create(
+            title="Setting up a project for testing",
+            author="moi",
+            slug="Setting-up-project",
+            description="sakfhésdhféakhfd",
+            url="",
+            image="",
+        )
+
+    def test_post_model(self):
+        d = self.blog
+        self.assertTrue(isinstance(d, Project))
+
+    def test_str(self):
+        d = self.blog
+        self.assertEqual(str(d), "Setting up a project for testing")
+
+
+class ViewTesting(TestCase):
+    def setUp(self):
+        self.blog = Project.objects.create(
+            title="Setting up a project for testing",
+            author="moi",
+            slug="Setting-up-project",
+            description="sakfhésdhféakhfd",
+            url="",
+            image="",
+        )
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get("/projects/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_project_index_uses_correct_template(self):
+        response = self.client.get("/projects/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "projects/project_index.html")
+
+    def test_view_project_detail_uses_correct_template(self):
+        response = self.client.get("/projects/1/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "projects/project_detail.html")
